0b26a1164697b2f6c9bb2f8c4d74af67b74b1232
Alex Schouls <alex_schouls@live.com>
2020-04-29 08:35:28 +0200

    Renamed projects

--- API/source/API/Startup.cs
+++ API/source/API/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace VRIS.API
+namespace API
 {
     public class Startup
     {
