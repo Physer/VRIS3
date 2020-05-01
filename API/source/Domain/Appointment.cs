f42b1eda26ab0564f71d720f67deef58e2889f74
Alex Schouls <alex_schouls@live.com>
2020-05-01 08:58:11 +0200

    Added a calendar controller, repository and models

---
+++ API/source/Domain/Appointment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Appointment
+    {
+        public int AppointmentId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Owner { get; set; }
+        public IEnumerable<string> Attendees { get; set; }
+    }
+}
---
+++ API/source/Domain/Calendar.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Calendar
+    {
+        public int CalendarId { get; set; }
+        public string Owner { get; set; }
+        public IEnumerable<Appointment> Appointments { get; set; } = new List<Appointment>();
+    }
+}
